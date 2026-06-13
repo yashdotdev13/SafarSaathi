0b5e0949e8097f132c5efa29b45752e5c1755d57
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-13 11:07:31 +0530

    updated the global exception handler

--- auth-service/src/main/java/com/company/SafarSaathi/auth_service/exceptions/ApiError.java
+++ auth-service/src/main/java/com/company/SafarSaathi/auth_service/exceptions/ApiError.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 public class ApiError {
 
-    private LocalDateTime timeStamp;
+    private LocalDateTime timestamp;
     private int status;
 
     private String error;
