1143d59582851b19b426cd97a7db708f30645b82
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-15 12:23:58 +0530

    feat(common-module): create shared event contracts and kafka topics

--- common-module/src/main/java/com/company/SafarSaathi/common/constants/KafkaTopics.java
+++ common-module/src/main/java/com/company/SafarSaathi/common/constants/KafkaTopics.java
@@ -1,4 +1,4 @@
-package constants;
+package com.company.SafarSaathi.common.constants;
 
 
 public final class KafkaTopics {
--- common-module/src/main/java/com/company/SafarSaathi/common/enums/EventType.java
+++ common-module/src/main/java/com/company/SafarSaathi/common/enums/EventType.java
@@ -1,4 +1,4 @@
-package enums;
+package com.company.SafarSaathi.common.enums;
 
 public enum EventType {
 
