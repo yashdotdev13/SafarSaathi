908c5378dd601ef06affe44dd05c7207906296e7
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-17 09:29:20 +0530

    refracted the repository in the companion-service

--- companion-service/src/main/java/com/company/SafarSaathi/companion_service/repository/CompanionRepository.java
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/repository/CompanionRepository.java
@@ -1,16 +1,19 @@
 package com.company.SafarSaathi.companion_service.repository;
 
 import com.company.SafarSaathi.companion_service.entity.Companion;
+import com.company.SafarSaathi.companion_service.enums.CompanionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CompanionRepository extends JpaRepository<Companion,Long> {
+public interface CompanionRepository
+        extends JpaRepository<Companion, Long> {
 
     List<Companion> findByTripId(Long tripId);
 
     List<Companion> findByUserId(Long userId);
 
-    List<Companion> findByStatus(String open);
-
+    List<Companion> findByStatus(
+            CompanionStatus status
+    );
 }
--- companion-service/src/main/java/com/company/SafarSaathi/companion_service/repository/CompanionRequestRepository.java
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/repository/CompanionRequestRepository.java
@@ -1,18 +1,37 @@
 package com.company.SafarSaathi.companion_service.repository;
 
 import com.company.SafarSaathi.companion_service.entity.CompanionRequest;
+import com.company.SafarSaathi.companion_service.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface CompanionRequestRepository extends JpaRepository<CompanionRequest,Long> {
+public interface CompanionRequestRepository
+        extends JpaRepository<CompanionRequest, Long> {
 
+    List<CompanionRequest> findByReceiverId(
+            Long receiverId
+    );
 
-    List<CompanionRequest> findByReceiverId(Long receiverId);
+    List<CompanionRequest> findBySenderId(
+            Long senderId
+    );
 
-    List<CompanionRequest> findBySenderId(Long senderId);
+    List<CompanionRequest> findByReceiverIdAndStatus(
+            Long receiverId,
+            RequestStatus status
+    );
 
-    Optional<CompanionRequest> findBySenderIdAndReceiverIdAndTripId(Long senderId, Long receiverId, Long tripId);
+    List<CompanionRequest> findBySenderIdAndStatus(
+            Long senderId,
+            RequestStatus status
+    );
 
+    Optional<CompanionRequest>
+    findBySenderIdAndReceiverIdAndTripId(
+            Long senderId,
+            Long receiverId,
+            Long tripId
+    );
 }
