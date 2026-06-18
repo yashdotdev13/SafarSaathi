2e5dfe326a02e0f9d1d5561522bf145ddb82e5a1
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-18 15:41:19 +0530

    created the feign config class

--- companion-service/src/main/java/com/company/SafarSaathi/companion_service/client/UserServiceClient.java
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/client/UserServiceClient.java
@@ -1,12 +1,14 @@
 package com.company.SafarSaathi.companion_service.client;
 
+import com.company.SafarSaathi.companion_service.config.FeignConfig;
 import com.company.SafarSaathi.companion_service.dtos.external.UserProfileResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "USER-SERVICE"
+        name = "USER-SERVICE",
+        configuration = FeignConfig.class
 )
 public interface UserServiceClient {
 
