7aec03d65c1985bc5d734eb32c34f10353fa6389
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-19 15:56:06 +0530

    created the notification service

---
+++ notification-service/src/main/java/com/company/SafarSaathi/notification_service/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.company.SafarSaathi.notification_service.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
--- notification-service/src/main/java/com/company/SafarSaathi/notification_service/service/NotificationService.java
+++ notification-service/src/main/java/com/company/SafarSaathi/notification_service/service/NotificationService.java
@@ -1,7 +1,11 @@
 package com.company.SafarSaathi.notification_service.service;
 
 
+import com.company.SafarSaathi.notification_service.auth.UserContextHolder;
 import com.company.SafarSaathi.notification_service.dtos.NotificationResponse;
+import com.company.SafarSaathi.notification_service.entity.Notification;
+import com.company.SafarSaathi.notification_service.exceptions.BadRequestException;
+import com.company.SafarSaathi.notification_service.exceptions.ResourceNotFoundException;
 import com.company.SafarSaathi.notification_service.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,14 +18,79 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-@Transactional
 public class NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final ModelMapper modelMapper;
 
 
+    @Transactional(readOnly = true)
     public List<NotificationResponse> getNotifications(){
-        Long userId  = UserCon
+        Long userId  = UserContextHolder.getCurrentUserId();
+
+        return notificationRepository
+                .findByUserIdOrderByCreatedAtDesc(userId)
+                .stream()
+                .map(notification ->
+                        modelMapper.map(
+                                notification,
+                                NotificationResponse.class
+                        ))
+                .toList();
     }
+
+    @Transactional(readOnly = true)
+    public List<NotificationResponse> getUnreadNotifications(){
+
+        Long userId = UserContextHolder.getCurrentUserId();
+
+        return notificationRepository.findByUserIdAndReadFalseOrderByCreatedAtDesc(
+                userId
+        )
+                .stream()
+                .map(notification ->
+                        modelMapper.map(
+                                notification,
+                                NotificationResponse.class
+                        ))
+                .toList();
+    }
+
+
+    public NotificationResponse markAsRead(
+            Long notificationId
+    ) {
+
+        Long currentUserId =
+                UserContextHolder.getCurrentUserId();
+
+        Notification notification =
+                notificationRepository.findById(notificationId)
+                        .orElseThrow(() ->
+                                new ResourceNotFoundException(
+                                        "Notification not found"
+                                )
+                        );
+
+        if (!notification.getUserId()
+                .equals(currentUserId)) {
+
+            throw new BadRequestException(
+                    "You are not authorized to update this notification"
+            );
+        }
+
+        notification.setRead(true);
+
+        Notification saved =
+                notificationRepository.save(notification);
+
+        return modelMapper.map(
+                saved,
+                NotificationResponse.class
+        );
+    }
+
+
+
 }
