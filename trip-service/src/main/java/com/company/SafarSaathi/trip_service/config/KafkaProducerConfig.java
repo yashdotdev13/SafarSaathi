cb6663902a030602ee9e251c21b04415fbbfb6cb
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-23 01:38:59 +0530

    created the kafka topics and kafka producer config class in the trip-service

---
+++ trip-service/src/main/java/com/company/SafarSaathi/trip_service/config/KafkaProducerConfig.java
@@ -0,0 +1,56 @@
+package com.company.SafarSaathi.trip_service.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(
+            KafkaProperties kafkaProperties
+    ) {
+
+        Map<String, Object> config =
+                new HashMap<>();
+
+        config.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaProperties.getBootstrapServers()
+        );
+
+        config.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+
+        config.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                JsonSerializer.class
+        );
+
+        return new DefaultKafkaProducerFactory<>(
+                config
+        );
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(
+            ProducerFactory<String, Object> producerFactory
+    ) {
+
+        return new KafkaTemplate<>(
+                producerFactory
+        );
+    }
+}
