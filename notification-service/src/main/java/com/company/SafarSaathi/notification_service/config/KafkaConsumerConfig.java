082f850dc833e0f7252d8988c297a412603b3be6
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-23 02:05:46 +0530

    added the kafka consumer config for the trip event in the notification-service

--- notification-service/src/main/java/com/company/SafarSaathi/notification_service/config/KafkaConsumerConfig.java
+++ notification-service/src/main/java/com/company/SafarSaathi/notification_service/config/KafkaConsumerConfig.java
@@ -3,8 +3,10 @@ package com.company.SafarSaathi.notification_service.config;
 import com.company.SafarSaathi.common.events.NotificationEvent;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -14,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConsumerConfig {
 
     @Bean
@@ -67,6 +70,63 @@ public class KafkaConsumerConfig {
 
         factory.setConsumerFactory(
                 consumerFactory()
+        );
+
+        return factory;
+    }
+
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(
+            KafkaProperties kafkaProperties
+    ) {
+
+        JsonDeserializer<Object> deserializer =
+                new JsonDeserializer<>();
+
+        deserializer.addTrustedPackages("*");
+
+        Map<String, Object> props =
+                new HashMap<>();
+
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                kafkaProperties.getBootstrapServers()
+        );
+
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "notification-service-group"
+        );
+
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class
+        );
+
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class
+        );
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                deserializer
+        );
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object>
+    kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory
+    ) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(
+                consumerFactory
         );
 
         return factory;
