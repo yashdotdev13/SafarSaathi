539d0f710d8352c08975f944a0414f8fa100be1d
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-24 08:47:18 +0530

    recommendation response dto in the companion-service

---
+++ companion-service/src/main/java/com/company/SafarSaathi/companion_service/dtos/response/RecommendationResponse.java
@@ -0,0 +1,22 @@
+package com.company.SafarSaathi.companion_service.dtos.response;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RecommendationResponse {
+
+    private Long userId;
+
+    private Long tripId;
+
+    private String fullName;
+
+    private Integer matchScore;
+
+    private String travelStyle;
+
+    private String tripMode;
+}
