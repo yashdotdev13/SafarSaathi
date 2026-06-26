53211139ec9ef10768b7eb90d27596ac63a16540
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-27 01:44:03 +0530

    created the PromptType enum

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/prompt/PromptType.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/prompt/PromptType.java
@@ -1,4 +1,15 @@
 package com.company.SafarSaathi.ai_service.prompt;
 
-public class PromptType {
+public enum PromptType {
+
+    CHAT,
+    TRIP_PLANNED,
+    COMPANION_MATCH,
+
+    DESTINATION_GUIDE,
+    ITINERARY,
+
+    BUDGET,
+
+    PACKING
 }
