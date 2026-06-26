adbb3bac6fd47386e04ad5206e62dc62b26a0ec2
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-27 01:36:35 +0530

    end to end workflow is working

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/controller/AiController.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/controller/AiController.java
@@ -1,7 +1,5 @@
 package com.company.SafarSaathi.ai_service.controller;
 
-
-
 import com.company.SafarSaathi.ai_service.dtos.ChatRequest;
 import com.company.SafarSaathi.ai_service.dtos.ChatResponse;
 import com.company.SafarSaathi.ai_service.service.AiAssistantService;
