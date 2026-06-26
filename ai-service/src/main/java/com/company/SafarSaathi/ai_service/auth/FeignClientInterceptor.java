fbb2ddbd435ad9510de71db731a7dd32e2a450a4
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-26 08:56:46 +0530

    Added the auth package for the centralized authentication

---
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/auth/FeignClientInterceptor.java
@@ -0,0 +1,22 @@
+package com.company.SafarSaathi.ai_service.auth;
+
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class FeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        Long userId= UserContextHolder.getCurrentUserId();
+
+        if(userId!=null){
+            requestTemplate.header("X-User-Id",userId.toString());
+        }
+
+    }
+}
