9b40d4cae62ad23b48e80650b6dabcf8cbd76f14
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-26 09:00:13 +0530

    created the aiConfig and feign config

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/AiServiceApplication.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/AiServiceApplication.java
@@ -2,8 +2,10 @@ package com.company.SafarSaathi.ai_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AiServiceApplication {
 
 	public static void main(String[] args) {
---
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/config/AiConfig.java
@@ -0,0 +1,7 @@
+package com.company.SafarSaathi.ai_service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfig {
+}
