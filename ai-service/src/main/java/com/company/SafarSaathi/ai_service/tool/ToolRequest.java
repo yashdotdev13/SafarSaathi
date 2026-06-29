0213ed42d8cb6f3beaa56f1730858be6f4baaf90
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-29 09:39:20 +0530

    created the ToolRequest class

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/tool/ToolRequest.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/tool/ToolRequest.java
@@ -1,4 +1,26 @@
 package com.company.SafarSaathi.ai_service.tool;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ToolRequest {
+
+    private ToolType toolType;
+    private Long userId;
+
+    private String conversationId;
+
+    private String query;
+
+    @Builder.Default
+    private Map<String, Object> parameters = Map.of();
 }
