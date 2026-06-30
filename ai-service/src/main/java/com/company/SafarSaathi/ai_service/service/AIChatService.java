3d915b4f34262f386fab28b83c3fd990776a16ce
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-30 10:36:28 +0530

    moved the ai assistant service to the ai chat serviceImpl

---
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/service/AIChatService.java
@@ -0,0 +1,13 @@
+package com.company.SafarSaathi.ai_service.service;
+
+
+import com.company.SafarSaathi.ai_service.dtos.ChatRequest;
+import com.company.SafarSaathi.ai_service.dtos.ChatResponse;
+
+public interface AIChatService {
+
+    ChatResponse chat(
+            ChatRequest request
+    );
+
+}
---
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/service/impl/AIChatServiceImpl.java
@@ -0,0 +1,91 @@
+package com.company.SafarSaathi.ai_service.service.impl;
+
+import com.company.SafarSaathi.ai_service.auth.UserContextHolder;
+import com.company.SafarSaathi.ai_service.conversation.entity.Conversation;
+import com.company.SafarSaathi.ai_service.conversation.entity.ConversationMessage;
+import com.company.SafarSaathi.ai_service.conversation.enums.MessageRole;
+import com.company.SafarSaathi.ai_service.conversation.service.ConversationService;
+import com.company.SafarSaathi.ai_service.dtos.ChatRequest;
+import com.company.SafarSaathi.ai_service.dtos.ChatResponse;
+import com.company.SafarSaathi.ai_service.prompt.PromptBuilderService;
+import com.company.SafarSaathi.ai_service.prompt.PromptType;
+import com.company.SafarSaathi.ai_service.service.AIChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AIChatServiceImpl implements AIChatService {
+
+
+    private final ChatClient chatClient;
+    private final PromptBuilderService promptBuilderService;
+    private final ConversationService conversationService;
+
+    @Override
+    public ChatResponse chat(ChatRequest request) {
+
+        log.info("Received AI chat request.");
+
+        Long userId = UserContextHolder.getCurrentUserId();
+
+        Conversation conversation =
+                conversationService.getOrCreateConversation(
+                        userId,
+                        request.getConversationId()
+                );
+
+        conversationService.saveMessage(
+                conversation,
+                MessageRole.USER,
+                request.getMessage()
+        );
+
+        List<ConversationMessage> history =
+                conversationService.getConversationHistory(
+                        conversation
+                );
+
+        String prompt =
+                promptBuilderService.buildPrompt(
+                        PromptType.CHAT,
+                        request,
+                        history
+                );
+
+        log.debug(
+                "Generated prompt with {} conversation messages.",
+                history.size()
+        );
+
+        String aiResponse =
+                chatClient.prompt()
+                        .user(prompt)
+                        .call()
+                        .content();
+
+        conversationService.saveMessage(
+                conversation,
+                MessageRole.ASSISTANT,
+                aiResponse
+        );
+
+        log.info(
+                "AI response generated successfully. conversationId={}",
+                conversation.getConversationId()
+        );
+
+        return ChatResponse.builder()
+                .conversationId(
+                        conversation.getConversationId().toString()
+                )
+                .response(aiResponse)
+                .build();
+    }
+}
