de0d55a6816c527c1eda26bb27e000f64deb1365
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-06-30 10:24:36 +0530

    created the skeleton of the helper method

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/orchestrator/Impl/AIOrchestratorServiceImpl.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/orchestrator/Impl/AIOrchestratorServiceImpl.java
@@ -3,6 +3,7 @@ package com.company.SafarSaathi.ai_service.orchestrator.Impl;
 
 import com.company.SafarSaathi.ai_service.dtos.ChatRequest;
 import com.company.SafarSaathi.ai_service.dtos.ChatResponse;
+import com.company.SafarSaathi.ai_service.intent.IntentDetectionResult;
 import com.company.SafarSaathi.ai_service.intent.IntentDetectionService;
 import com.company.SafarSaathi.ai_service.orchestrator.AIOrchestratorService;
 import com.company.SafarSaathi.ai_service.tool.ToolExecutor;
@@ -25,6 +26,20 @@ public class AIOrchestratorServiceImpl implements AIOrchestratorService {
     public ChatResponse process(ChatRequest request) {
 
         log.info("Processing request through AI orchestrator");
+        return null;
+    }
+
+
+    private ChatResponse handleGeneralChat(ChatRequest request) {
+
+        log.info("Handling General Chat.");
+        return null;
+    }
+
+
+    private ChatResponse handleToolRequest(ChatRequest request,IntentDetectionResult intent) {
+
+        log.info("Handling Tool Request for {}",intent.getIntentType());
         return null;
     }
 }
