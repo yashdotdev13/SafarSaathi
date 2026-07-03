db3aa0da5bccacaa1bc13a09117b394c97be6f36
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-07-03 09:58:44 +0530

    add: Tool metadara class

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/tool/ToolMetadata.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/tool/ToolMetadata.java
@@ -1,4 +1,16 @@
 package com.company.SafarSaathi.ai_service.tool;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ToolMetadata {
+
+    private ToolType toolType;
+
+    private String description;
+
+    private boolean enabled;
 }
