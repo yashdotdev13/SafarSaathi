07c15902443d897a2c7af94ca2d0c15d07e81fee
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-07-10 02:50:53 +0530

    added the MemoryRepository

---
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/memory/repositories/MemoryRepository.java
@@ -0,0 +1,34 @@
+package com.company.SafarSaathi.ai_service.memory.repositories;
+
+
+import com.company.SafarSaathi.ai_service.memory.Memory;
+import com.company.SafarSaathi.ai_service.memory.enums.MemoryCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemoryRepository extends JpaRepository<Memory, Long> {
+
+    List<Memory> findByUserId(Long userId);
+
+    List<Memory> findByUserIdOrderByCategoryAscAttributeAsc(Long userId);
+
+    List<Memory> findByUserIdAndCategory(
+            Long userId,
+            MemoryCategory category
+    );
+
+    Optional<Memory> findByUserIdAndCategoryAndAttribute(
+            Long userId,
+            MemoryCategory category,
+            String attribute
+    );
+
+    boolean existsByUserIdAndCategoryAndAttribute(
+            Long userId,
+            MemoryCategory category,
+            String attribute
+    );
+
+}
