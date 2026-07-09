319e8c390456e2254b6b2e8cbb73606f94ea40fd
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-07-10 02:28:34 +0530

    updated the orechestrator-service

--- ai-service/src/main/java/com/company/SafarSaathi/ai_service/planner/Impl/AIPlannerImpl.java
+++ ai-service/src/main/java/com/company/SafarSaathi/ai_service/planner/Impl/AIPlannerImpl.java
@@ -1,45 +1,30 @@
 package com.company.SafarSaathi.ai_service.planner.Impl;
 
 
+
 import com.company.SafarSaathi.ai_service.context.model.ConversationContext;
 import com.company.SafarSaathi.ai_service.planner.AIPlanner;
 import com.company.SafarSaathi.ai_service.planner.dto.ExecutionPlan;
-import com.company.SafarSaathi.ai_service.planner.dto.PlannedTool;
-import com.company.SafarSaathi.ai_service.planner.enums.ExecutionStrategy;
-import com.company.SafarSaathi.ai_service.planner.rules.RuleEvaluator;
+import com.company.SafarSaathi.ai_service.planner.service.PlannerEngine;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class AIPlannerImpl implements AIPlanner {
+public class AIPlannerImpl
+        implements AIPlanner {
 
-    private final RuleEvaluator ruleEvaluator;
+    private final PlannerEngine planningEngine;
 
     @Override
     public ExecutionPlan createPlan(
             ConversationContext context
     ) {
 
-        log.info("Creating execution plan.");
+        log.info("Delegating planning request to Planning Engine.");
+        return planningEngine.createExecutionPlan(context);
 
-        List<PlannedTool> plannedTools =
-                ruleEvaluator.evaluate(context);
-
-        ExecutionPlan executionPlan = ExecutionPlan.builder()
-                .strategy(ExecutionStrategy.SEQUENTIAL)
-                .tools(plannedTools)
-                .build();
-
-        log.info(
-                "Execution plan created with {} tool(s).",
-                plannedTools.size()
-        );
-
-        return executionPlan;
     }
 }
