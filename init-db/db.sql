820a072d05171edf85f24eef55d99096ae4c196f
Yash chauhan <yashchauhan.gaya@gmail.com>
2026-07-17 00:01:53 +0530

    add: docker-compose added

---
+++ init-db/db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE userdb_safarsaathi;
+
+CREATE DATABASE safar_userdb;
+
+CREATE DATABASE safar_tripdb;
+
+CREATE DATABASE safar_companiondb;
+
+CREATE DATABASE safar_notificationdb;
+
+CREATE DATABASE ai_service_db;
